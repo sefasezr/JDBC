634554af4997729c708e338eb076de7835191a3f
sefasezr <153550790+sefasezr@users.noreply.github.com>
2024-07-16 10:54:13 +0300

    16.07.2024

--- src/main/java/jdbc_day01/ExecuteQuery01.java
+++ src/main/java/jdbc_day01/ExecuteQuery01.java
@@ -5,38 +5,28 @@ import java.sql.*;
 public class ExecuteQuery01 {
     public static void main(String[] args) throws SQLException {
         //!!! 2.ADIM : Hangi DB , Hangi kullanici ve sifre
-        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/B254JDBC_nt","B254nt","password");
+        Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/B254JDBC_nt","B254nt","password");
 
         //!!! 3.ADIM: statement olusturma, yazacagimiz query'leri
-        // statement nesnesi ile DB ye gonderecegiz
-        Statement st = con.createStatement();
+        //statement nesnesi ile DB ye gonderecegiz
+        Statement st=con.createStatement();
         System.out.println("success");
 
-        //!!! 4.ADIM: sorguları olusturma
-        st.execute("CREATE TABLE if not exists countries\n" +
-                "        ( id SERIAL PRIMARY KEY\n" +
-                "         , country_name VARCHAR(100) );");
-        /*st.execute("INSERT INTO countries\n" +
-                "                (id,country_name) VALUES\n" +
-                "                (1,'Turkey'), (2,'Germany'), (3,\n" +
-                "                'France'),\n" +
-                "        (4,'United States'),\n" +
-                "        (5,'Canada'),\n" +
-                "        (6,'Japan'),\n" +
-                "        (7,'Brazil'),\n" +
-                "        (8,'Australia'),\n" +
-                "        (9,'India'),\n" +
-                "        (10,'United Kingdom');"); */
+        //!!! 4.ADIM: sorgulari olusturma
         //!!!   ÖRNEK 1:id'si 5 ile 10 arasında olan ülkelerin "country_name" bilgisini listeleyiniz.
-
-        String query1 = "SELECT country_name FROM countries WHERE id BETWEEN 5 AND 10 ";
-        boolean sql1 = st.execute(query1);
-        System.out.println("sql1 = "+sql1);
+        String query1="select country_name from countries where id in(5,6,7,8,9,10)";
+        boolean sql1=st.execute(query1);
+        System.out.println("sql1 = " + sql1);//true
 
         // Verileri almak istersem :
-        ResultSet rs = st.executeQuery(query1);
-        while (rs.next()) {
-            System.out.println("Ulke Adı : "+rs.getString("country_name"));
+        ResultSet rs=st.executeQuery(query1);
+        int a=1;
+        while (rs.next()){
+            System.out.println(a+". Ulke Adı : "+rs.getString("country_name"));
+            a++;
         }
+        //!!! 5.ADIM kaynaklarımızı kapatıyor
+        st.close();
+        con.close();
     }
 }
